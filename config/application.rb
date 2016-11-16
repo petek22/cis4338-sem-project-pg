8e09466abf1e66128b7ecaed3d02c14d3c2cc4e9
Pete Kamenszky <petek_22@outlook.com>
2016-11-16 13:08:08 -0600

    added code to application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.serve_static_assets = true
+
 
 module SemProjCis4339
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
   end
 end
