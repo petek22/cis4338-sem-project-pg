1dad1fc554e3389cd47df792bee80617ba31f912
Pete Kamenszky <petek_22@outlook.com>
2016-11-16 14:34:14 -0600

    fixed image problem

--- Gemfile
+++ Gemfile
@@ -47,7 +47,7 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console'
-  gem  'rails_12factor'
+
 
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.assets.compile = true
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -23,7 +24,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
